d889461a9d8c5bb53823ee3828e0efd7aaa65fa8
Sam <samroman3@gmail.com>
2019-10-08 13:44:37 -0400

    Adds tableview programmatically

--- Project/ProgrammaticUserCells/ProgrammaticUserCells/ViewController.swift
+++ Project/ProgrammaticUserCells/ProgrammaticUserCells/ViewController.swift
@@ -1,8 +1,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var users = [User]() {
+        didSet{
+            userTableView.reloadData()
+        }
+    }
+    
+    
+    
+    lazy var userTableView: UITableView = {
+        let theTableView = UITableView()
+        theTableView.dataSource = self
+        let nib = UINib(nibName: "userTableViewCell", bundle: nil)
+        theTableView.register(nib, forCellReuseIdentifier: "userTableViewCell")
+        //cells - we've created a cell variable that dequeues a cell IB item that we created in storyboard
+        //
+        return theTableView
+    }()
+    
+    
+    func loadUsers(){
+        UsersFetchingService.manager.getUsers { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .failure(let error):
+                    print(error)
+                case .success(let data):
+                    self.users = data
+                }
+            }
+        }
+    }
 
     override func viewDidLoad() {
+        self.view.addSubview(userTableView)
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
@@ -10,3 +43,18 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+}
